c69879611fb4cd7d2881652afc120a7a5a8d2a9f
UmangLadha <umangladha02@gmail.com>
2021-08-28 18:13:05 +0530

    JavaScript added

---
+++ aap.js
@@ -0,0 +1,16 @@
+var input = document.querySelector("#input-txt");
+var button = document.querySelector("#btn-translator");
+var output = document.querySelector("#output-txt");
+var url = "https://api.funtranslations.com/translate/ebonics.json";
+function geturl(txt)
+{
+    return url + "?text=" + txt;
+}
+ function clickhandler()
+{
+    var txt = input.value;
+     fetch(geturl(txt))
+     .then(respones => respones.json())
+     .then(json => output.innerText = json.contents.translated);
+};
+button.addEventListener("click", clickhandler);
